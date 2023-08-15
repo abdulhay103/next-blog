60144899f570a91791d971886293f3b4e6f4b4b4
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-15 10:30:21 +0600

    page setup done

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <h1 className=" text-lg text-orange-600 font-bold">Page is loading...</h1>
+    </div>
+  );
+};
+
+export default loading;
